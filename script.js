265af541565d602ad54415bf429c650ee0399ff9
Krazy <48533302+ItWasMeProductions@users.noreply.github.com>
2022-08-03 14:34:50 -0700

    update1

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    const inputText = document.getElementById('input-text');
+    const buttonText = document.getElementById('button-text');
+
+    const button = document.getElementById('button');
+
+    UpdateSizeInfo(document.getElementById('device-size'));
+
+    inputText.addEventListener('input', () => UpdateButtonText(inputText, buttonText));
+
+    button.addEventListener('click', () => Speak(inputText));
+}
+
+function UpdateButtonText(_text, _button) {
+    _button.innerHTML = _text.value;
+}
+
+function Speak(_text) {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = _text.value;
+    window.speechSynthesis.speak(msg);
+}
+
+window.onresize = () => UpdateSizeInfo(document.getElementById('device-size'));
+
+function UpdateSizeInfo(sizeInfoText) {
+    const colSize = (window.innerWidth > 1400) ? 'xxl' : (window.innerWidth > 1200) ? 'xl' : (window.innerWidth > 992) ? 'lg' : (window.innerWidth > 768) ? 'md' : (window.innerWidth > 576) ? 'sm' : 'xs';
+    sizeInfoText.innerHTML = `Width: ${window.innerWidth}, Height: ${window.innerHeight}, Column Size: ${colSize}`;
+}
